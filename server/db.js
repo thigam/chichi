2c643feffc2e78a08b22b1d89d9f3988036d29d6
Thigs <matthewthiga72@gmail.com>
2025-05-10 22:28:45 +0200

    Fixed ssl error with render

--- server/db.js
+++ server/db.js
@@ -5,6 +5,9 @@ require('dotenv').config(); // make sure it can read DATABASE_URL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   // you can also add ssl: { rejectUnauthorized: false } if needed in production
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
